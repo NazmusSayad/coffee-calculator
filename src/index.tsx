f2f57e03e18305d61e346fcea9eec26e594c53a7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-11-28 16:53:49 +0600

    hudai

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,12 @@ import { createRoot } from 'react-dom/client'
 import 'css-reset-plus'
 import './styles/index.scss'
 import App from './App'
-import ErrorBoundary from 'error-boundary-react'
 
 const rootElement = document.getElementById('Root')!
 const root = createRoot(rootElement)
 
 root.render(
-  <ErrorBoundary element={<h1>Error</h1>}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ErrorBoundary>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
